27434c3bcf26c50f8be3a06cae521d4079769535
Vivek jetani <jetanivivek321@gmail.com>
2025-03-25 18:19:34 +0530

    add helloworld page

---
+++ src/app/helloworld/page.js
@@ -0,0 +1,9 @@
+export default function HelloWorldPage() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-green-900 to-blue-900 text-white">
+        <h1 className="text-4xl font-bold">👋 Hello, World!</h1>
+        <p className="mt-4 text-lg">Hello World!</p>
+      </div>
+    );
+  }
+  
