7726a9e3e079875001c1e3840213cfd0ef81c103
Vivek jetani <jetanivivek321@gmail.com>
2025-03-25 16:55:46 +0530

    remove remaining lec filed

--- src/app/page.js
+++ src/app/page.js
@@ -31,7 +31,7 @@ export default function Page() {
 
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:5000/calculate", {
+      const response = await fetch("https://how-many-bunk-i-can-have-backend.vercel.app/calculate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
